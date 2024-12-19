8687e09d3a9df7196cdf0929e6645d107d928d09
gldrdev <geovaniluizdarosa@gmail.com>
2024-12-19 16:25:54 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+document.getElementById("loginForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+  
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+  
+    // Validação simples
+    if (username === "admin" && password === "1234") {
+      document.getElementById("loginMessage").textContent = "Login bem-sucedido!";
+      document.getElementById("loginMessage").style.color = "green";
+      // Redirecionar ou carregar conteúdo
+      setTimeout(() => {
+        window.location.href = "../html/home.html";
+      }, 1000);
+    } else {
+      document.getElementById("loginMessage").textContent = "Usuário ou senha incorretos!";
+    }
+  });
+  
